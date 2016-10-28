771d676b194e5fae6595de982ef65b9c42d16eb3
Nicholas Porter <njporter10@gmail.com>
2016-10-28 16:29:03 -0700

    added in logos to the photos

--- builds/templates/home.php
+++ builds/templates/home.php
@@ -22,7 +22,10 @@
             <!--Featured Photo-->
             <div class="col-xs-12 col-sm-10 ">
                 <span class="photo-of-day"> Photo of the Day | Salticidae Spider</span>
-                <img class="main-img" src="/micropics/builds/images/Microscope Photos/Bugs/Spiders/SmallRedSpiderCH4.jpg" width="100%">
+                <div class="main-img">
+                    <img src="/micropics/builds/images/logos/LogoWhite.png" height="55px" style="position:absolute; right:0;margin:5px 44px 0 0">
+                    <img src="/micropics/builds/images/Microscope Photos/Bugs/Spiders/SmallRedSpiderCH4.jpg" width="100%">
+                </div>
                 <div class="description">
                     <strong> Description:</strong> This spider was found in Southern California and is part of
                     the<strong> Salticidae Family (Jumping Spider)</strong>.
@@ -30,11 +33,14 @@
                     It is hanging on the end of a piece of 0.7mm lead from a pencil.<br><br>
                     For more photos and videos of this spider <a>click here</a>.
                 </div>
+
             </div>
             <!-- Your share button code -->
-            <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5812f025935d1de8"></script>             <!--            <a href="https://twitter.com/share" data-show-count="false"><img class="twitter-logo" src="/micropics/builds/images/Twitter_logo_initial.png"></a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>-->
-<!--            <br>-->
-<!--            <a href="http://www.facebook.com/sharer.php?u=www.micro-pics.com&t=title"><img class="facebook-logo" src="/micropics/builds/images/facebook-logo.png"></a>-->
+            <script type="text/javascript"
+                    src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5812f025935d1de8"></script>
+            <!--            <a href="https://twitter.com/share" data-show-count="false"><img class="twitter-logo" src="/micropics/builds/images/Twitter_logo_initial.png"></a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>-->
+            <!--            <br>-->
+            <!--            <a href="http://www.facebook.com/sharer.php?u=www.micro-pics.com&t=title"><img class="facebook-logo" src="/micropics/builds/images/facebook-logo.png"></a>-->
         </div>
     </div>
     <div class="row">
@@ -65,7 +71,7 @@
     <div class="row">
         <div class=" hidden-xs col-sm-3"></div>
         <div class="col-sm-9">
-            <div class="col-xs-12 col-sm-4"  data-aos="flip-up">
+            <div class="col-xs-12 col-sm-4" data-aos="flip-up">
                 <div class="photo-box">
                     <img src="/micropics/builds/images/Microscope Photos/Bugs/Spiders/SmallRedSpiderCH1.jpg"
                          width="100%">
@@ -77,7 +83,7 @@
                          width="100%">
                 </div>
             </div>
-            <div class="col-xs-12 col-sm-4"  data-aos="flip-up">
+            <div class="col-xs-12 col-sm-4" data-aos="flip-up">
                 <div class="photo-box">
                     <img src="/micropics/builds/images/Microscope Photos/Bugs/Spiders/SmallRedSpiderCH3.jpg"
                          width="100%">
@@ -94,33 +100,33 @@
             <img src="/micropics/builds/images/spider-eyes-diagram.jpg"
                  width="100%">
             <div class="spider-list">
-            <ul>
-                <li><strong>Family Lycosidae</strong> – the Wolf Spiders</li>
-                <li><strong>Family Salticidae</strong> – the Jumping Spiders</li>
-                <li><strong>Family Salticidae, genus Lyssomanes</strong> – the Magnolia Green Jumpers</li>
-                <li><strong>Family Araneidae</strong> – the Orbweavers</li>
-                <li><strong>Family Pisauridae, genus Dolomedes</strong> – the Fishing Spiders</li>
-                <li><strong>Family Pisauridae, genus Pisaurina</strong> – the Nursery Web Spiders</li>
-                <li><strong>Family Ctenidae</strong> – the Wandering Spiders</li>
-                <li><strong>Family Oxyopidae</strong> – the Lynx Spiders</li>
-                <li><strong>Family Philodromidae</strong> – the Running Crab Spiders</li>
-                <li><strong>Family Dysderidae</strong> – the Woodlouse Hunters</li>
-                <li><strong> Family Tetragnathidae, genus Tetragnatha</strong> – the Longjawed Orbweavers</li>
-                <li><strong>Family Thomisidae, genus Xysticus</strong> – the Ground Crab Spiders</li>
-                <li><strong>Family Agelenidae, genus Eratigena</strong> – the Funnel Weavers</li>
-                <li><strong>Family Agelenidae, genus Agelenopsis</strong> – the Grass Spiders (aka Funnel Weavers)</li>
-                <li><strong>Family Selenopidae, genus Selenops</strong> – the Flatties (aka Crab Spiders)</li>
-                <li><strong>Family Sparassidae, genus Heteropoda</strong> – the Huntsman (aka Giant Crab Spiders)</li>
-                <li><strong>Family Sparassidae, genus Olios</strong> – Giant Crab Spiders (aka Huntsman)</li>
-                <li><strong>Family Sicariidae, genus Loxosceles</strong> – the Brown Spiders (includes the Brown Recluse)</li>
-                <li><strong>Family Uloboridae, genus Hyptiotes</strong> – the Triangle Weavers</li>
-                <li><strong>Family Zoropsidae, species Zoropsis spinimana</strong> – the False Wolf Spider</li>
-                <li><strong>Family Deinopidae, species Deinopis spinosa</strong> – the Net-casting Spider (aka Ogre-faced Spider); note that the four other eyes are not visible from the front.</li>
-                <li><strong>Family Diguetidae, genus Diguetia</strong> – the Desertshrub Spiders</li>
-                <li><strong>Family Antrodiaetidae, genus Antrodiaetus</strong> – the Folding-door Spider (aka Turret Spiders)</li>
-                <li><strong>Family Segestriidae</strong> – the Tube Web Spiders</li>
-                <li><strong>Family Scytodidae</strong> – the Spitting Spiders</li>
-            </ul>
+                <ul>
+                    <li><strong>Family Lycosidae</strong> – the Wolf Spiders</li>
+                    <li><strong>Family Salticidae</strong> – the Jumping Spiders</li>
+                    <li><strong>Family Salticidae, genus Lyssomanes</strong> – the Magnolia Green Jumpers</li>
+                    <li><strong>Family Araneidae</strong> – the Orbweavers</li>
+                    <li><strong>Family Pisauridae, genus Dolomedes</strong> – the Fishing Spiders</li>
+                    <li><strong>Family Pisauridae, genus Pisaurina</strong> – the Nursery Web Spiders</li>
+                    <li><strong>Family Ctenidae</strong> – the Wandering Spiders</li>
+                    <li><strong>Family Oxyopidae</strong> – the Lynx Spiders</li>
+                    <li><strong>Family Philodromidae</strong> – the Running Crab Spiders</li>
+                    <li><strong>Family Dysderidae</strong> – the Woodlouse Hunters</li>
+                    <li><strong>Family Tetragnathidae, genus Tetragnatha</strong> – the Longjawed Orbweavers</li>
+                    <li><strong>Family Thomisidae, genus Xysticus</strong> – the Ground Crab Spiders</li>
+                    <li><strong>Family Agelenidae, genus Eratigena</strong> – the Funnel Weavers</li>
+                    <li><strong>Family Agelenidae, genus Agelenopsis</strong> – the Grass Spiders (aka Funnel Weavers)</li>
+                    <li><strong>Family Selenopidae, genus Selenops</strong> – the Flatties (aka Crab Spiders)</li>
+                    <li><strong>Family Sparassidae, genus Heteropoda</strong> – the Huntsman (aka Giant Crab Spiders)</li>
+                    <li><strong>Family Sparassidae, genus Olios</strong> – Giant Crab Spiders (aka Huntsman)</li>
+                    <li><strong>Family Sicariidae, genus Loxosceles</strong> – the Brown Spiders (includes the Brown Recluse)</li>
+                    <li><strong>Family Uloboridae, genus Hyptiotes</strong> – the Triangle Weavers</li>
+                    <li><strong>Family Zoropsidae, species Zoropsis spinimana</strong> – the False Wolf Spider</li>
+                    <li><strong>Family Deinopidae, species Deinopis spinosa</strong> – the Net-casting Spider (Ogre-faced Spider);four other eyes are not front visible</li>
+                    <li><strong>Family Diguetidae, genus Diguetia</strong> – the Desertshrub Spiders</li>
+                    <li><strong>Family Antrodiaetidae, genus Antrodiaetus</strong> – the Folding-door Spider (aka Turret Spiders)</li>
+                    <li><strong>Family Segestriidae</strong> – the Tube Web Spiders</li>
+                    <li><strong>Family Scytodidae</strong> – the Spitting Spiders</li>
+                </ul>
             </div>
         </div>
     </div>
